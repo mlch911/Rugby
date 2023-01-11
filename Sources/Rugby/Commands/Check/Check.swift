426bdcf3a4724c592bf2d601eab32fe035a3c816
mlch911 <mlch911@mluoc.tech>
2023-01-11 14:06:53 +0800

    Update Check

--- Sources/Rugby/Commands/Check/Check.swift
+++ Sources/Rugby/Commands/Check/Check.swift
@@ -10,42 +10,42 @@ import Files
 import Foundation
 
 struct Check: ParsableCommand {
+	@Flag(help: .beta("Use content checksums instead of modification date.\n")) var useContentChecksums = false
+	
 	static var configuration = CommandConfiguration(
-		abstract: "• Remove cache except \("plans.yml".yellow) and logs."
+		abstract: "• Check if there is any changes in local Pods. If so, throw an error."
 	)
 	
 	func run() throws {
 		try WrappedError.wrap(playBell: false) {
-			try CheckStep().run()
+			try CheckStep(command: self).run()
 		}
 	}
 }
 
 struct CheckStep: Step {
 	let progress: Printer
+	private let checksumsProvider: ChecksumsProvider
 	
-	init() {
+	init(command: Check) {
 		self.progress = RugbyPrinter(title: "Check", verbose: .verbose, quiet: false, nonInteractive: false)
+		self.checksumsProvider = ChecksumsProvider(useContentChecksums: command.useContentChecksums)
 	}
 	
 	func run(_ input: Void) throws {
 		let cache = CacheManager().load()
-		let changedPods = try PodsProvider.shared.localPods()
-			.filter { !$0.isGitClean }
-		guard let pods = try cache?.values.first?.checksums?
-			.compactMap({ text -> (String, NSTextCheckingResult)? in
-				guard let result = try ".*(?=:\\ )".regex().firstMatch(text) else { return nil }
-				return (text, result)
-			}).map({ ($0.0 as NSString).substring(with: $0.1.range) }) else {
-			done()
-			return
+		let localPods = try PodsProvider.shared.localPods()
+		guard let checksums = cache?.values.first?.checksumsMap() else { return }
+		let localPodsChecksums = try progress.spinner("Calculate checksums") {
+			try checksumsProvider.getChecksums(forPods: Set(localPods.map(\.name)))
 		}
-		if let pod = pods.first(where: changedPods.map(\.name).contains) {
-			progress.print("[Check Failed] Found changed Pod: \(pod) in Cache.")
-			throw CheckError.checkFail
-		} else {
+		let changedPods = localPodsChecksums.filter { checksums[$0.name] != nil && checksums[$0.name]?.value != $0.value }
+		if changedPods.isEmpty {
 			progress.print("[Check Passed]")
 			done()
+		} else {
+			progress.print("[Check Failed] Found changed Pod: \(changedPods) in Cache.")
+			throw CheckError.checkFail
 		}
 	}
 }
