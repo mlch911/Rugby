83172b5e6ba873f50d2b2c2aa89b9987ea415f07
mlch911 <mlch911@mluoc.tech>
2024-04-03 11:39:09 +0800

    Bump version 1.30.11

--- Formula/rugby.rb
+++ Formula/rugby.rb
@@ -1,7 +1,7 @@
 class Rugby < Formula
   desc "🏈 Cache CocoaPods for faster rebuild and indexing Xcode project"
   homepage "https://github.com/swiftyfinch/Rugby"
-  version "1.30.10"
+  version "1.30.11"
   url "https://github.com/swiftyfinch/Rugby/releases/download/#{version}/rugby.zip"
 
   def install
